824f25e584433f3822eb797ae48c290e68e208f4
Leena S N <leenasn@gmail.com>
2017-12-05 12:57:30 +0530

    Make the title as a param
    - Increase the size of the logo by 3X
    - Center align the title of the receipt, increase the font size for title

--- lib/receipts/receipt.rb
+++ lib/receipts/receipt.rb
@@ -3,7 +3,7 @@ require 'prawn/table'
 
 module Receipts
   class Receipt < Prawn::Document
-    attr_reader :attributes, :id, :company, :custom_font, :line_items, :logo, :message, :product
+    attr_reader :attributes, :id, :company, :custom_font, :line_items, :logo, :message, :product, :title
 
     def initialize(attributes)
       @attributes  = attributes
@@ -12,6 +12,7 @@ module Receipts
       @line_items  = attributes.fetch(:line_items)
       @custom_font = attributes.fetch(:font, {})
       @message     = attributes.fetch(:message) { default_message }
+      @title       = attributes.fetch(:title){ default_title }
 
       super(margin: 0)
 
@@ -23,6 +24,10 @@ module Receipts
 
       def default_message
         "We've received your payment for #{attributes.fetch(:product)}. You can keep this receipt for your records. For questions, contact us anytime at <color rgb='326d92'><link href='mailto:#{company.fetch(:email)}?subject=Charge ##{id}'><b>#{company.fetch(:email)}</b></link></color>."
+      end
+
+      def default_title
+        "RECEIPT FOR CHARGE ##{id}"
       end
 
       def setup_fonts
@@ -44,13 +49,13 @@ module Receipts
         move_down 60
 
         if company.has_key? :logo
-          image open(company.fetch(:logo)), height: 32
+          image open(company.fetch(:logo)), height: 96
         else
           move_down 32
         end
 
         move_down 8
-        text "<color rgb='a6a6a6'>RECEIPT FOR CHARGE ##{id}</color>", inline_format: true
+        text "<color rgb='a6a6a6'>#{title}</color>", inline_format: true, size: 24, :align => :center
 
         move_down 30
         text message, inline_format: true, size: 12.5, leading: 4
