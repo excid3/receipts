e4321c8503b2d6af178448421d622bae3b96d74f
Joseph Joshua <joejoshw@gmail.com>
2022-02-20 14:35:26 +0100

    Improve payslip design

--- lib/receipts/payslip.rb
+++ lib/receipts/payslip.rb
@@ -1,6 +1,10 @@
 module Receipts
     class Payslip < Base
       @title = "Payslip"
+      
+      def default_message(company:)
+        "For questions, contact us anytime at <color rgb='326d92'><link href='mailto:#{company.fetch(:email)}?subject=Question about my receipt'><b>#{company.fetch(:email)}</b></link></color>."
+      end
     end
   end
   
